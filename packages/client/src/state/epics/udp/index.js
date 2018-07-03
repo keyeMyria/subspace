c152c471affd0add0d6f197505e7cd2a5d4f7ea3
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2018-07-02 22:31:35 -0400

    docs: added comments to Udp epics

--- packages/client/src/state/epics/udp/index.js
+++ packages/client/src/state/epics/udp/index.js
@@ -24,12 +24,14 @@ const stateHandlers = {
 const client = new UdpClient({
   url: `ws://${window.location.host}/server`,
 })
+// Observable that provides the UDP connection with local messages
 const input = ReplaySubject.create()
 
 export function connect(action$, store) {
   return action$.pipe(
     ofType(Udp.CONNECT),
     mergeMap(action => {
+      // Create a UDP connection and exchange auth info during handshake
       const metadata = { token: Auth.getToken(store.getState()) }
       const { messages, status } = udpConnection(
         input,
@@ -39,7 +41,9 @@ export function connect(action$, store) {
       )
 
       return merge(
+        // Emit actions when the connection state changes
         status.pipe(map(state => stateHandlers[state]())),
+        // Handle messages until connection closes
         messages.pipe(
           map(data => Udp.receive(JSON.parse(data))),
           takeUntil(action$.pipe(ofType(Udp.CLOSE))),
@@ -52,8 +56,11 @@ export function connect(action$, store) {
 export function send(action$) {
   return action$.pipe(
     ofType(Udp.SEND),
+    // Send messages to the remote connection using the input subject
     tap(action => input.next(JSON.stringify(action.payload))),
+    // Stop sending messages once connection closes
     takeUntil(action$.pipe(ofType(Udp.CLOSE))),
+    // No-op
     ignoreElements(),
   )
 }
